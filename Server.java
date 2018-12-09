2d1f5bb3572aabaf9d9be390701a7020bca7faab
Christian Davi <32940205+cdbm@users.noreply.github.com>
2018-12-09 17:44:14 -0200

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,27 +1,77 @@
-import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Arrays;
 
 public class Server {
-	public static void main(String args[]) throws IOException {
-		DatagramSocket serverSocket = new DatagramSocket(9876);
-		byte[] recData = new byte[1024];
-		int i = 0;
+	static int serverPort;
+	static String filename;
 
-		FileOutputStream file = new FileOutputStream(
-				"D:/Users/cdbm/Documents/testing-received.7z");
-		
+	public static void main(String args[]) throws SocketException, IOException {
+		int count = 0;
+		int MAX_SIZE = 1048;
 
+		DatagramSocket clientSocket = new DatagramSocket();
+		InetAddress IpAddress = InetAddress.getByName("localhost");
 
-		while (true) {
-			
+		byte[] sendData = new byte[MAX_SIZE];
 
-			DatagramPacket recPacket = new DatagramPacket(recData, recData.length);
-			serverSocket.receive(recPacket);
-			file.write(recPacket.getData());
-			System.out.println("\nPacket" + ++i + " written to file\n");
-			file.flush();
+		String filePath = "C:/Users/C. Davi/Documents/Lista_2.txt";
+		File file = new File(filePath);
+		FileInputStream fis = new FileInputStream(file);
+
+		int totLength = 0;
+
+		while ((count = fis.read(sendData)) != -1) // calculate total length of file
+		{
+			totLength += count;
 		}
+
+		System.out.println("Total Length :" + totLength);
+
+		int noOfPackets = totLength / MAX_SIZE;
+		System.out.println("No of packets : " + noOfPackets);
+
+		int off = noOfPackets * MAX_SIZE; // calculate offset. it total length of file is 1048 and array size is 1000
+											// den starting position of last packet is 1001. this value is stored in
+											// off.
+
+		int lastPackLen = totLength - off;
+		System.out.println("\nLast packet Length : " + lastPackLen);
+
+		byte[] lastPack = new byte[lastPackLen ]; // create new array without redundant information
+
+		fis.close();
+
+		FileInputStream fis1 = new FileInputStream(file);
+		// while((count = fis1.read(sendData)) != -1 && (noOfPackets!=0))
+		while ((count = fis1.read(sendData)) != -1) {
+			if (noOfPackets <= 0)
+				break;
+			System.out.println(new String(sendData));
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IpAddress, 9876);
+			clientSocket.send(sendPacket);
+			System.out.println("========");
+			System.out.println("last pack sent" + sendPacket);
+			noOfPackets--;
+		}
+
+		// check
+		System.out.println("\nlast packet\n");
+		System.out.println(new String(sendData));
+
+		lastPack = Arrays.copyOf(sendData, lastPackLen);
+
+		System.out.println("\nActual last packet\n");
+		System.out.println(new String(lastPack));
+		// send the correct packet now. but this packet is not being send.
+		DatagramPacket sendPacket1 = new DatagramPacket(lastPack, lastPack.length, IpAddress, 9876);
+		clientSocket.send(sendPacket1);
+		System.out.println("last pack sent" + sendPacket1);
+
 	}
 }
