6596a45da36e07286ec1374cefbf7a3bfe2ca202
AdrianoCabral <32920783+AdrianoCabral@users.noreply.github.com>
2018-12-11 22:36:11 -0300

    Add files via upload

--- Server.java
+++ Server.java
@@ -10,6 +10,7 @@ public class Server {
 	static int serverPort;
 	static String filename;
 	static boolean[] window = new boolean[10];
+	static int sendBase = 0, lastSent;
 	static int MAX_SIZE = 1048;
 	static String sn;
 	static Timer timeout;
@@ -21,7 +22,6 @@ public class Server {
 		byte[] recData = new byte[2];
 		byte[] sendData = new byte[MAX_SIZE];
 		String ack;
-		
 
 		String filePath = "C:/Users/diani/Downloads/Lista_2.txt";
 		File file = new File(filePath);
@@ -53,28 +53,40 @@ public class Server {
 		FileInputStream fis1 = new FileInputStream(file);
 		// while((count = fis1.read(sendData)) != -1 && (noOfPackets!=0))
 		int sequenceNum = 0;
+		boolean b = true;
+		
+		// byte[] trusend = Arrays.copyOf(sendData, MAX_SIZE+5);
+		for (int i = 0; i < 10; i++) {
+			if (noOfPackets <= 0)
+				break;
+			send(sendData, sequenceNum);
+			sequenceNum++;
+			if (sequenceNum > 20)
+				sequenceNum = 0;
+			noOfPackets--;
+		}
 		while ((count = fis1.read(sendData)) != -1) {
 			if (noOfPackets <= 0)
 				break;
-			// byte[] trusend = Arrays.copyOf(sendData, MAX_SIZE+5);
-			for (int i = 0; i < 10; i++) {
+			recData = new byte[2];
+			DatagramPacket recPacket = new DatagramPacket(recData, recData.length);
+			if (noOfPackets <= 0)
+				break;
+			serverSocket.receive(recPacket);
+			ack = new String(recPacket.getData(), "UTF-8");
+			ackNum = Integer.parseInt(ack);
+			window[ackNum % 10] = true;
+			if (ackNum == sendBase) {
+				sendBase++;
+				if(sendBase > 20)
+						sendBase= 0;
 				send(sendData, sequenceNum);
 				sequenceNum++;
 				if(sequenceNum > 20)
-					sequenceNum = 0;
+						sequenceNum++;
 				noOfPackets--;
 			}
-			
-			while(true) {
-				recData = new byte[2];
-				DatagramPacket recPacket = new DatagramPacket(recData, recData.length);
-				if(noOfPackets <= 0)
-						break;
-				serverSocket.receive(recPacket);
-				ack = new String(recPacket.getData(), "UTF-8");
-				ackNum = Integer.parseInt(ack);
-				window[ackNum%10] = true;
-			}
+
 		}
 
 		// check
@@ -113,14 +125,14 @@ public class Server {
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IpAddress, 9876);
 		clientSocket.send(sendPacket);
 		timeout.schedule(new PacketTimeout(seqNum, sendData), 3500);
-		window[seqNum%10] = false;
+		window[seqNum % 10] = false;
 		System.out.println(sendPacket.hashCode());
 		System.out.println("========");
 		System.out.println("last pack sent     " + new String(sendPacket.getData()));
 		clientSocket.close();
 	}
 
-	class PacketTimeout extends TimerTask {
+	static class PacketTimeout extends TimerTask {
 		private int seq;
 		private byte[] message;
 
